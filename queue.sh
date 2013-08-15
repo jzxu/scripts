12a633dddd3df0fb7be13522a87a02d44c82df57
Joseph Xu <josephzxu@gmail.com>
2013-08-15 15:22:01 -0400

    change queue.sh to use files instead of variables to hold pid lists

--- queue.sh
+++ queue.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-del_elem() {
-	for e in $1
-	do
-		if [ "$e" != "$2" ]
-		then
-			echo -n "$e "
-		fi
-	done
-}
-
 stopped() {
 	stat=`ps -o pid,stat | grep $1 | awk '{print substr($2,1,1)}'`
 	[ "$stat" = T ]
@@ -31,107 +21,154 @@ make_queue() {
 			rm -rf "$1"
 			;;
 		*)
-			return 1
+			exit 1
 			;;
 		esac
 	fi
 	
-	QUEUE_DIR="$1"
-	mkdir -p $QUEUE_DIR || { echo "directory creation failed" 1>&2; exit 1; }
-	QUEUE_FIFO="$QUEUE_DIR/fifo"
-	mkfifo $QUEUE_FIFO || { echo "fifo creation failed" 1>&2; exit 1; }
+	queue_dir="$1"
+	mkdir -p $queue_dir || { echo "queue directory creation failed" 1>&2; exit 1; }
+	fifo="$queue_dir/fifo"
+	mkfifo $fifo || { echo "fifo creation failed" 1>&2; exit 1; }
 	
 	if [ -z "$2" ]
 	then
-		QUEUE_MAX_PROCS=1
+		echo 1 >"$queue_dir/nprocs"
 	else
-		QUEUE_MAX_PROCS="$2"
+		echo "$2" >"$queue_dir/nprocs"
 	fi
-	WAITING_PIDS=""
 }
 
 clear_queue() {
-	echo Killing remaining jobs: $ACTIVE_PIDS $WAITING_PIDS 1>&2
-	kill -9 $ACTIVE_PIDS $WAITING_PIDS
+	if [ ! -d "$1" ]
+	then
+		echo "specify queue directory" >&2
+		exit 1
+	fi
+	queue="$1"
+	run_pids=`cat "$queue/running"`
+	wait_pids=`cat "$queue/waiting"`
+	echo Killing remaining jobs: $run_pids $wait_pids 1>&2
+	kill -9 $run_pids $wait_pids
 }
 
 exit_queue() {
-	echo `date` exit $2 >>"$JOB_DIR/status"
-	echo exit $1 $2 >$QUEUE_FIFO
+	queue="$1"
+	pid="$2"
+	code="$3"
+	job_dir="$queue/$pid"
+	echo `date` exit $code >>"$job_dir/status"
+	echo exit $pid $code >"$queue/fifo"
 }
 
 init_job() {
-	trap "exit_queue $BASHPID \$?" EXIT
+	if [ ! -d "$1" ]
+	then
+		echo "specify queue directory" >&2
+		exit 1
+	fi
+	queue="$1"
+
+	trap "exit_queue $queue $BASHPID \$?" EXIT
 	
-	JOB_DIR="$QUEUE_DIR/$BASHPID"
-	mkdir "$JOB_DIR" || { echo "Cannot create $JOB_DIR"; exit 1; }
-	exec 1>"$JOB_DIR/stdout"
-	exec 2>"$JOB_DIR/stderr"
+	job_dir="$queue/$BASHPID"
+	mkdir "$job_dir" || { echo "Cannot create $job_dir"; exit 1; }
+	exec 1>"$job_dir/stdout"
+	exec 2>"$job_dir/stderr"
 	
 	if [ -n "$*" ]
 	then
-		echo $* >"$JOB_DIR/label"
+		echo $* >"$job_dir/label"
 	fi
-	echo `date` queued >>"$JOB_DIR/status"
+	echo `date` queued >>"$job_dir/status"
 	kill -s SIGSTOP $BASHPID
-	echo `date` running >>"$JOB_DIR/status"
+	echo `date` running >>"$job_dir/status"
 }
 
 reg_queue() {
-	WAITING_PIDS="$WAITING_PIDS $!"
+	pid=$!
+	if [ ! -d "$1" ]
+	then
+		echo "specify queue directory" >&2
+		exit 1
+	fi
+	echo $pid >>$1/waiting
 }
 
 wait_queue() {
-	if [ -z "$WAITING_PIDS" ]
+	if [ ! -d "$1" ]
+	then
+		echo "specify queue directory" >&2
+		exit 1
+	fi
+
+	queue="$1"
+	fifo="$queue/fifo"
+	wait_list="$queue/waiting"
+	run_list="$queue/running"
+	done_list="$queue/done"
+	fail_list="$queue/failed"
+	nprocs=`cat $queue/nprocs`
+
+	if [ ! -f "$wait_list" ]
 	then
 		return 0
 	fi
 	
-	trap "{ clear_queue; exit 1; }" SIGINT
+	trap "{ clear_queue $queue; exit 1; }" SIGINT
 	
-	ACTIVE_PIDS=""
-	NACTIVE=0
-	BAD_PIDS=""
-	set $WAITING_PIDS
-	while [ $# -gt 0 -o $NACTIVE -gt 0 ]
+	nwait=`wc -l <"$wait_list"`
+	nrun=0
+	while [ "$nwait" -gt 0 -o "$nrun" -gt 0 ]
 	do
-		while [ "$NACTIVE" -lt $QUEUE_MAX_PROCS -a "$#" -gt 0 ]
+		while [ "$nrun" -lt $nprocs -a "$nwait" -gt 0 ]
 		do
-			ACTIVE_PIDS="$ACTIVE_PIDS $1"
-			NACTIVE=$((NACTIVE+1))
+			pid=`head -n 1 "$wait_list"`
 			
 			# make sure process is stopped before issuing SIGCONT
-			while ! stopped $1
+			while ! stopped $pid
 			do
 				sleep 1
 			done
-			kill -s SIGCONT $1
-			shift
+			kill -s SIGCONT $pid
+
+			nwait=$((nwait-1))
+			nrun=$((nrun+1))
+			sed -i "/^$pid\$/d" "$wait_list"
+			echo $pid >>"$run_list"
 		done
-		WAITING_PIDS="$*"
-		echo "        " $NACTIVE running, $# queued
-		badstatus=0
+		printf "        %3d running, %3d queued\n" $nrun $nwait
+
 		while read event pid status
 		do
 			case "$event" in
 			exit)
-				if [ $status != 0 ]
+				if ! grep -q "$pid" "$run_list"
 				then
-					echo "WARNING: Job $pid exited with status $status" 1>&2
-					badstatus=$status
-					BAD_PIDS="$BAD_PIDS $pid"
+					echo "WARNING: unknown job $pid exited with status $status" 1>&2
+				else
+					if [ $status != 0 ]
+					then
+						echo "WARNING: Job $pid exited with status $status" 1>&2
+						echo $pid >>"$fail_list"
+					else
+						echo $pid >>"$done_list"
+					fi
+					sed -i "/^$pid\$/d" "$run_list"
+					nrun=$((nrun-1))
 				fi
-				ACTIVE_PIDS=`del_elem "$ACTIVE_PIDS" $pid`
-				NACTIVE=$((NACTIVE-1))
 				;;
 			esac
-		done <$QUEUE_FIFO
+		done <"$fifo"
 	done
-	if [ -n "$BAD_PIDS" ]
+	if [ -f "$fail_list" ]
 	then
-		echo "These processes had nonzero exit status: " $BAD_PIDS
-		set $BAD_PIDS
-		return $#
+		failed=`cat $fail_list`
+		echo "These processes had nonzero exit status: " $failed
+		nfail=`wc -l <"$fail_list"`
+		return $nfail
 	fi
+
+	trap - SIGINT
 	return 0
 }
